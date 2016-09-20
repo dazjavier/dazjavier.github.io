dd4e243536afecf5d610fb78244edcb04cc1b550
Javier Diaz <javiercoree@gmail.com>
2016-09-20 15:26:53 -0300

    New version

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  Particles.init({
+    selector: '#myCanvas',
+    color: '#3a9148',
+    connectParticles: true,
+    minDistance: 150,
+    maxParticles: 100
+  });
+};

2ef5402d947035f091bb95fae350655b04af7c69
Javier Diaz <javiercoree@gmail.com>
2016-09-20 17:32:26 -0300

    fixed mobile

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
 window.onload = function() {
-  Particles.init({
-    selector: '#myCanvas',
+	var options = {};
+	var innerWidth = window.innerWidth;
+	if (innerWidth <= 320) { options = setMobileOptions(); }
+	if (innerWidth >= 480) { options = setNormalOptions(); }
+	console.log(innerWidth);
+	Particles.init(options);
+  
+};
+$('a').css({'z-index': '5', 'color': 'red'});
+
+function setMobileOptions() {
+	var o = {
+		selector: '#myCanvas',
     color: '#3a9148',
     connectParticles: true,
-    minDistance: 150,
-    maxParticles: 100
-  });
-};
+    minDistance: 50,
+    maxParticles: 50
+	};
+
+	return o;
+}
+
+function setNormalOptions() {
+	var o = {
+		selector: '#myCanvas',
+	  color: '#3a9148',
+	  connectParticles: true,
+	  minDistance: 150,
+	  maxParticles: 100
+	};
+
+	return o;
+}
